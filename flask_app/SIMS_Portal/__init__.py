8b9f866eb8ddcd045dbc185935ecf59a6ff4a111
Jonathan Garro <jonathan.garro@gmail.com>
2022-06-04 15:30:35 +0000

    new achievement badges

--- flask_app/SIMS_Portal/__init__.py
+++ flask_app/SIMS_Portal/__init__.py
@@ -15,6 +15,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
