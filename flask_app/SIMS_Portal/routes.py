b773c1be52d12317288e6d425b25a95612b186d5
Jonathan Garro <jonathan.garro@gmail.com>
2022-06-04 13:18:28 +0000

    password reset language update

--- flask_app/SIMS_Portal/routes.py
+++ flask_app/SIMS_Portal/routes.py
@@ -271,7 +271,7 @@ def reset_token(token):
 		return redirect(url_for('dashboard'))
 	user = User.verify_reset_token(token)
 	if user is None:
-		flash('An invalid or expired token.', 'warning')
+		flash('That is an invalid or expired token.', 'warning')
 		return redirect(url_for('reset_request'))
 	form = ResetPasswordForm()
 	if form.validate_on_submit():
